d6e414b22e47f9b87caab308b8261b8a89823a5c
zaruudon524 <16546@kitakama.ed.jp>
2021-03-22 17:43:15 +0900

    blogの一覧

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,7 +8,8 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     public function index(Post $post)
-{
-         return $post->get();
+    {
+         return view('index')->with(['posts' => $post->get()]);
+    }
 }
-}
+
--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,5 @@
 |
 */
 
-Route::get('/posts', 'PostController@index');
+Route::get('/', 'PostController@index');
+
