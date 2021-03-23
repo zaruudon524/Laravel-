d79d85b952d45c48990b9b3ccc662fd8b13855ef
zaruudon524 <16546@kitakama.ed.jp>
2021-03-24 02:06:03 +0900

    日付日時の降順で最新10件

--- app/Post.php
+++ app/Post.php
@@ -6,6 +6,9 @@ use App\Post;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
-{
-    //
+{              
+    public function getByLimit(int $limit_count = 10)
+    {
+    return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
+    }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,16 +4,18 @@
         <meta charset="utf-8">
         <title>Blog</title>
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     
     <body>
         <h1>Blog Name</h1>
         <div class='posts'>
-            <div class='posts'>
-                <h2 class='title'>Title 1</h2>
-                <P class='body'>This is a sample body.</p>
-            </div>
+            @foreach ($posts as $post)
+                <div class='post'>
+                    <h2 class='title'>{{ $post->title }}</h2>
+                    <p class='body'>{{ $post->body }}</p>
+                </div>
+            @endforeach
         </div>
     </body>
 </html>
