1eac7848c5dba171257bb3b3fb4981d3e97881eb
zaruudon524 <16546@kitakama.ed.jp>
2021-04-05 21:04:12 +0900

    削除

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -10,11 +10,25 @@
     <body>
         <h1>Blog Name</h1
         <p class="edit">[<a href="/posts/{{ $post->id }}/edit">edit</a>]</p>
+        <form action="/posts/{{ $post->id }}"  id="form_delete" method="post">
+            @csrf
+            @method('delete')
+            <input type="submit" style="display:none">
+            <p class='delete'>[<span onclick="return deletePost(this);">delete</span>]</p>
+        </form>
             <div class='post'>
                 <h2 class='title'>{{ $post->title }}</h2>
                 <p class='body'>{{ $post->body }}</p>
                 <p class='updated_at'>{{ $post->updated_at }}</p>
             </div>
             <div class="back">[<a href="/">back</a>]</div>
+            <script>
+                function deletePost(e) {
+                    'use strict';
+                    if (confirm('削除すると復元できません。\n本当に削除しますか？'))　{
+                        document.getElementById('form_delete').submit();
+                    }
+                }
+            </script>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,6 @@ Route::get('/', 'PostController@index');
 Route::get('/posts/create', 'PostController@create');
 Route::get('/posts/{post}/edit', 'PostController@edit');
 Route::put('/posts/{post}', 'PostController@update');
+Route::delete('/posts/{post}', 'PostController@delete');
 Route::get('/posts/{post}', 'PostController@show');
 Route::post('/posts', 'PostController@store');
